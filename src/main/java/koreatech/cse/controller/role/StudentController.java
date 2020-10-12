2f365170a89e05c7fee80157e6271c11d4fcc6b8
CHANMYUNG KIM <cm.kim@thinkonweb.com>
2020-10-12 10:47:52 +0900

    a

--- src/main/java/koreatech/cse/controller/role/StudentController.java
+++ src/main/java/koreatech/cse/controller/role/StudentController.java
@@ -189,7 +189,7 @@ public class StudentController {
     private List<Integer> getYearList() {
         DateTime dt = new DateTime();
         int currentYear = dt.getYear();
-        List<Integer> yearList = new ArrayList<Integer>();
+        List<Integer> yearList = new ArrayList<>();
         for(int i=currentYear; i>=(currentYear - 10); i--) {
             yearList.add(i);
         }
